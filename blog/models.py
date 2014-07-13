715a66b87d1bf401c2cd8a62c3e4d5266db8c18c
Adam <yangzhenxi@Adams-MacBook-Air.local>
2014-07-12 23:51:04 -0700

    media file config

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
 # Defines Post data model
-"""
 class Post(models.Model):
 	title = models.CharField(max_length=30)
 	date = models.DateField()
-	img_url = models.ImageField()
-	thumbnail_url = models.ImageField()
+	image = models.ImageField(upload_to="blog/static/blog/media/images/%Y/%m/%d")
+	thumbnail = models.ImageField(upload_to="blog/static/blog/media/thumbnails/%Y/%m/%d")
 	content = models.TextField()
 	preview = models.TextField()
-"""
