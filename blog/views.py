6f9b47c4639faf52434e63928ad42d2fcacaeb3e
Adam <yangzhenxi@Adams-MacBook-Air.local>
2014-07-17 22:49:52 -0700

    worked on blog page

--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,14 @@ from django.template import RequestContext, loader
 from blog import models
 
 def index(request):
+	posts = models.Post.objects.all()
+
 	template = loader.get_template('blog/index.html')
-	context = RequestContext(request, {})
+	context = RequestContext(request, {'posts': posts})
 	return HttpResponse(template.render(context))
 
 def article(request):
-	post_0 = models.Post.objects.all()[1]
+	post_0 = models.Post.objects.all()[0]
 	print post_0.image.url
 	template = loader.get_template('blog/article.html')
 	context = RequestContext(request, {'image': post_0.image,
