34d2a0ca00eeb1da7ae61a69c9b440f3ad80e187
Adam <yangzhenxi@Adams-MacBook-Air.local>
2014-08-09 17:55:25 -0700

    setup S3 for production static file and media files

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 import blog
+
 admin.autodiscover()
 
 urlpatterns = [
 	url(r'^$', include(blog.urls)),
 	url(r'^blog/', include(blog.urls)),
     url(r'^admin/', include(admin.site.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
