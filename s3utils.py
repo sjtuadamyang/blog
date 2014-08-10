987732e991525d503a9683582b489563222d478f
Adam <yangzhenxi@Adams-MacBook-Air.local>
2014-08-10 10:51:52 -0700

    updated requiremnets.txt

--- s3utils.py
+++ s3utils.py
@@ -1,11 +1,4 @@
 from storages.backends.s3boto import S3BotoStorage
 
-class FixedS3BotoStorage(S3BotoStorage):
-	def url(self, name):
-		url = super(FixedS3BotoStorage, self).url(name)
-		if name.endswith('/') and not url.endswith('/'):
-			url += '/'
-		return url
-
 StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
 MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
