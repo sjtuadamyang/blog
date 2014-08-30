77c63a426d6ca69b3c437823e39760419b9a15f2
Adam <yangzhenxi@Adams-MacBook-Air.local>
2014-08-30 09:06:40 -0700

    fix mobile css

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from blog import views
 
 urlpatterns = [
 	url(r'^$', views.base, name='index'),
+	url(r'^index$', views.index, name='index'),
 	url(r'^article/(?P<id>\d+)$', views.article_svc, name='article'),
 	url(r'^frame$', views.frame, name='frame'),
 	url(r'^base$', views.base, name='base'),
