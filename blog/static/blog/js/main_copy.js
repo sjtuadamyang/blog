7a523934f915bc565e45d756d3c4050f434b4729
Adam <yangzhenxi@Adams-MacBook-Air.local>
2014-09-01 11:53:04 -0700

    fixed some bug

--- blog/static/blog/js/main_copy.js
+++ blog/static/blog/js/main_copy.js
@@ -8,57 +8,63 @@ $(window).load(function() {
 /* Load content */
 $('.navigation > li').click(function(){
   $('.flex-active').removeClass('flex-active');
-  $(this).addClass('flex-active');
-  $('.content').load("/blog/" + $(this).index().toString(), function(){
-    // Load js for google map
-    "use strict";
-    if ($('.content > div').attr('id') == '4') {
-      $('.nivo-lbox').nivoLightbox({ effect: 'fade' });
-      $('.post').click(function(){
-        $('.content').load("/blog/article/" + $(this).attr('id')) 
-      })
-    }
-    if ($('.content > div').attr('id') == '5') {
-      var map = new GMaps({
-        el: "#map",
-        lat: 37.4047456,
-        lng: -122.0071204,
-        zoom: 15, 
-        zoomControl : true,
-        zoomControlOpt: {
-          style : "BIG",
-          position: "TOP_LEFT"
-        },
-        panControl : true,
-        streetViewControl : false,
-        mapTypeControl: false,
-        overviewMapControl: false
-      });
-            
-      var styles = [
-            {
-              stylers: [
-                { hue: "#00ffe6" },
-                { saturation: -100 }
-              ]
-            }
-      ];
-            
-      map.addStyle({
-            styledMapName:"Styled Map",
-            styles: styles,
-            mapTypeId: "map_style"  
-      });
-            
-      map.setStyle("map_style");
+  var clickedPage = $(this);
+  clickedPage.addClass('flex-active');
+  $('.content').fadeOut('fast', function() {
+    $('.content').load("/blog/" + clickedPage.index().toString(), function(){
+      // Load js for google map
+      "use strict";
+      if ($('.content > div').attr('id') == '4') {
+        $('.nivo-lbox').nivoLightbox({ effect: 'fade' });
+        $('.post').click(function(){
+          $('.content').load("/blog/article/" + $(this).attr('id')) 
+        })
+      }
+      $('.content').fadeIn('fast', function() {
+        if ($('.content > div').attr('id') == '5') {
+          var map = new GMaps({
+            el: "#map",
+            lat: 37.4047456,
+            lng: -122.0071204,
+            zoom: 15, 
+            zoomControl : true,
+            zoomControlOpt: {
+              style : "BIG",
+              position: "TOP_LEFT"
+            },
+            panControl : true,
+            streetViewControl : false,
+            mapTypeControl: false,
+            overviewMapControl: false
+          });
+                
+          var styles = [
+                {
+                  stylers: [
+                    { hue: "#00ffe6" },
+                    { saturation: -100 }
+                  ]
+                }
+          ];
+                
+          map.addStyle({
+                styledMapName:"Styled Map",
+                styles: styles,
+                mapTypeId: "map_style"  
+          });
+                
+          map.setStyle("map_style");
 
-      map.addMarker({
-        lat: 37.4047456,
-        lng: -122.0071204,
-        icon: $('#marker').attr('href')
+          map.addMarker({
+            lat: 37.4047456,
+            lng: -122.0071204,
+            icon: $('#marker').attr('href')
+          });
+        }
       });
-    }
+    })
   })
+  
 })
 
 
